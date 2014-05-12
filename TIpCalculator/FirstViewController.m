a8ff70fe149fb0f448b9e024b4f178e57df31e5f
Sumit Savla <susavla@paypal.com>
2014-05-11 17:19:51 -0700

    final changes

--- TIpCalculator/FirstViewController.m
+++ TIpCalculator/FirstViewController.m
@@ -9,6 +9,17 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billInput;
+@property (weak, nonatomic) IBOutlet UISlider *splitNumSlider;
+@property (weak, nonatomic) IBOutlet UILabel *splitNumLbl;
+@property (weak, nonatomic) IBOutlet UILabel *tipPctLbl;
+@property (weak, nonatomic) IBOutlet UIStepper *tipAmtStepper;
+@property (weak, nonatomic) IBOutlet UILabel *totalLbl;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmtLbl;
+@property (weak, nonatomic) IBOutlet UILabel *splitAmtLbl;
+
+- (IBAction)onTap:(id)sender;
+- (void) calculateValues;
 
 @end
 
@@ -17,13 +28,63 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self calculateValues];
 }
 
-- (void)didReceiveMemoryWarning
-{
+// on Slider change event
+- (IBAction)updateSliderLbl:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    NSUInteger val = slider.value;
+    self.splitNumLbl.text = [NSString stringWithFormat:@"%d",val];
+    [self calculateValues];
+}
+
+// on Stepper change event
+- (IBAction)updateTipAmtLbl:(UIStepper *)sender {
+    NSUInteger value = sender.value;
+    [self.tipPctLbl setText:[NSString stringWithFormat:@"%d%s", (int)value,"%"]];
+    [self calculateValues];
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidAppear:(BOOL)animated {    
+    // Load default values and update UI controls
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    int splitValue = [defaults integerForKey:@"splitDefault"];
+    if(splitValue == 0){
+        splitValue = 2;
+    }
+    self.splitNumLbl.text = [NSString stringWithFormat:@"%d", splitValue];
+    int tipValue = [defaults integerForKey:@"tipDefault"];
+    if(tipValue == 0){
+        tipValue = 10;
+    }
+    self.tipPctLbl.text = [NSString stringWithFormat:@"%d%s", tipValue, "%"];
+    
+    // Update values
+    self.splitNumSlider.value = splitValue;
+    self.tipAmtStepper.value = tipValue;
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:(YES)];
+    [self calculateValues];
+}
+
+- (void) calculateValues {
+    float billAmt = [self.billInput.text floatValue];
+    float tipPct = self.tipAmtStepper.value / 100;
+    float tipAmt = billAmt * tipPct;
+    float totalAmt = billAmt + tipAmt;
+    float splitAmt = totalAmt/[self.splitNumLbl.text intValue];
+    
+    self.tipAmtLbl.text = [NSString stringWithFormat:@"$%.2f", tipAmt];
+    self.totalLbl.text = [NSString stringWithFormat:@"$%.2f", totalAmt];
+    self.splitAmtLbl.text = [NSString stringWithFormat:@"$%.2f", splitAmt];
+}
+     
 @end
--- TIpCalculator/SecondViewController.m
+++ TIpCalculator/SecondViewController.m
@@ -9,6 +9,8 @@
 #import "SecondViewController.h"
 
 @interface SecondViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *splitDefault;
+@property (weak, nonatomic) IBOutlet UITextField *tipDefault;
 
 @end
 
@@ -17,11 +19,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Load Defaults
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    int splitValue = [defaults integerForKey:@"splitDefault"];
+    self.splitDefault.text = [NSString stringWithFormat:@"%d", splitValue];
+    int tipValue = [defaults integerForKey:@"tipDefault"];
+    self.tipDefault.text = [NSString stringWithFormat:@"%d%s", tipValue, "%"];
+	
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (IBAction)onTap:(id)sender {
+    // Save defaults
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger:[self.splitDefault.text intValue]forKey:@"splitDefault"];
+    [defaults setInteger:[self.tipDefault.text intValue] forKey:@"tipDefault"];
+    [defaults synchronize];
+    
+    [self.view endEditing:(YES)];
+    
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
